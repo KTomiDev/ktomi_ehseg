04a286d15cfdf0d204c7fe73f7fa0e3dd1b82006
Tomi <168299301+KTomiDev@users.noreply.github.com>
2024-07-06 16:32:21 +0200

    Felesleges egységek törlése

--- client.lua
+++ client.lua
@@ -5,11 +5,7 @@ local fasz = false
 --szukseges: esx_stauts
 
 
-
-print("szia,ezt a scriptet ktomi alkotta, van benne hiba meg igenytelen, de én szeretlek téged<3")
-
-
-
+print("by ktomi - Éhség script")
 
 
 Citizen.CreateThread(function()
@@ -22,34 +18,19 @@ Citizen.CreateThread(function()
                  exports['okokNotify']:Alert("Rendszer", "Éhes vagy!", 9000, 'warning')
       
                 -- print("geci")
-                
-            else
 
-            --     SetPedIsDrunk(plyPed, true)
-             --    AnimpostfxPlay("DrugsMichaelAliensFight", 10000001, true)
-             --    ShakeGameplayCam("DRUNK_SHAKE", 3.0)
-         --         exports["esx_notify"]:Notify("info", 3000, "Go Eat") -- You can change it to your message
-              --    exports['okokNotify']:Alert("fs", "Éhes vagy!", 5000, 'info')
             end
         end)
 
         TriggerEvent('esx_status:getStatus', 'thirst', function(status)
             if status.val < 50000 then
                 local plyPed = GetPlayerPed(-1)
-                exports['okokNotify']:Alert("Rendszer", "Szomjas vagy!", 9000, 'warning') 
-             --   SetPedIsDrunk(plyPed, true)
-         
-    else
-
-        
-               -- ShakeGameplayCam("DRUNK_SHAKE", 3.0)
-              --  AnimpostfxPlay("DrugsMichaelAliensFight", 10000001, true)
+                exports['okokNotify']:Alert("Rendszer", "Szomjas vagy!", 9000, 'warning')          
             end
         end)
 
         TriggerEvent('esx_status:getStatus', 'stress', function(status)
             if status.val > 100000 then
-            
                 exports['okokNotify']:Alert("Rendszer", "Stresszes vagy!", 6000, 'warning')
             end
         end)
